f726f57fa47f3e2aa8e0c284a984fdea25854b9e
RuCode <i.rcode@yandex.ru>
2015-05-28 22:29:06 +0300

    Усовершенствовал тестовую программу для работы с почтой

--- Тестовые программы/Работа с почтой/engine.mail.pas
+++ Тестовые программы/Работа с почтой/engine.mail.pas
@@ -1,3 +1,10 @@
+{/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                            Модуль для управления почтой по средствам IMAP
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  05.2015 - RuCode
+    * Нвчал разработку
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////}
+
 unit Engine.Mail;
 
 {$mode objfpc}{$H+}
@@ -5,7 +12,8 @@ unit Engine.Mail;
 interface
 
 uses
-  Classes, Dialogs, SysUtils, ExtCtrls, Forms, Controls, imapsend, ssl_openssl;
+  Classes, Dialogs, SysUtils, ExtCtrls, Forms, Controls, imapsend, ssl_openssl, synautil, synacode, synaicnv,
+  mimeinln, mimemess, mimepart, synachar;
 
 type
 
@@ -14,24 +22,50 @@ type
   TCustomMail = class(TObject)
   private
     fConnected: boolean;
-    fLogin: string;
+    fUserName: string;
     fPassword: string;
     fIMAPHost: string;
     fIMAPPort: integer;
     fIMAPClient: TIMAPSend;
-    function GetFullResult: string;
+    {:UIID выбранной директории}
+    fCurrentFolderUIID: integer;
+    {:Количество новых писем в текущей директории}
+    fCountOfNewMails: integer;
+    {:Количество писем в текущей директории}
+    fCountOfMailsInCurrentFolder: integer;
+    function GetCountOfNewMails: integer;
+    function GetFullResult: TStringList;
+    function GetResultString: string;
+    function GetSelectedFolder: string;
     procedure SetConnected(AValue: boolean);
+    procedure SetSelectedFolder(AFolderName: string);
   public
     constructor Create; virtual;
     destructor Destroy; override;
+
+    {:Узнать список директорий на сервере.}
     procedure GetFolderList(var ListFolders: TStringList);
+    {:Узнать список директорий на сервере.}
+
   public
-    property Login: string read fLogin write flogin; // переименовать
+    {:Имя пользователя для авторизации.}
+    property UserName: string read fUserName write fUserName;
+    {:Пароль для авторизации.}
     property Password: string write fPassword;
+    {:Адрес сервера IMAP.}
     property IMAPHost: string read fIMAPHost write fIMAPHost;
+    {:Порт на котором вертиться протокол IMAP.}
     property IMAPPort: integer read fIMAPPort write fIMAPPort;
+    {:Управление соединением и получение состояния соединения.}
     property Connected: boolean read fConnected write SetConnected;
-    property FullResult: string read GetFullResult;// переименовать
+    {:Узнать или указать текущую директорию.}
+    property SelectedFolder: string read GetSelectedFolder write SetSelectedFolder;
+    {: Количество новых писем}
+    property CountOfNewMails: integer read GetCountOfNewMails;
+    {:Результаты команды.}
+    property FullResult: TStringList read GetFullResult;
+    {:Результаты команды.}
+    property ResultString: string read GetResultString;
   end;
 
 implementation
@@ -39,40 +73,97 @@ implementation
 { TCustomMail }
 
 procedure TCustomMail.SetConnected(AValue: boolean);
+// Устанавливаем или сбрасываем соединение с сервером
 begin
   if fConnected = AValue then
     Exit;
-  fIMAPClient.TargetHost := IMAPHost;
-  fIMAPClient.TargetPort := IntToStr(IMAPPort);
-  fIMAPClient.UserName := Login;
-  fIMAPClient.Password := fPassword;
-  fConnected := fIMAPClient.Login;
+  if AValue then
+  begin
+    fIMAPClient.TargetHost := IMAPHost;
+    fIMAPClient.TargetPort := IntToStr(IMAPPort);
+    fIMAPClient.UserName := UserName;
+    fIMAPClient.Password := fPassword;
+    fConnected := fIMAPClient.Login;
+  end
+  else
+  begin
+    fIMAPClient.Logout;
+    fConnected := False;
+  end;
 end;
 
-function TCustomMail.GetFullResult: string;
+procedure TCustomMail.SetSelectedFolder(AFolderName: string);
+// Выбираем новую директорию
+begin
+  if fIMAPClient.SelectFolder(AFolderName) then
+  begin
+    fCountOfMailsInCurrentFolder := fIMAPClient.SelectedCount;
+    fCountOfNewMails := fIMAPClient.SelectedRecent;
+    fCurrentFolderUIID := fIMAPClient.SelectedUIDvalidity;
+  end;
+end;
+
+function TCustomMail.GetFullResult: TStringList;
+// Получим полный ответ от сервера
+begin
+  Result := fIMAPClient.FullResult;
+end;
+
+function TCustomMail.GetCountOfNewMails: integer;
+// Количество новых писем
+var
+  MessList: TStringList;
+begin
+  MessList := TStringList.Create;
+  if ImapClient.SearchMess('UNSEEN', MessList) then
+  begin
+    Result := MessList.Count;
+    fCountOfNewMails := Result;
+  end;
+  MessList.Free;
+end;
+
+function TCustomMail.GetResultString: string;
+// Статус команды
 begin
   Result := fIMAPClient.ResultString;
 end;
 
+function TCustomMail.GetSelectedFolder: string;
+// Определить текущею директорию
+begin
+  Result := fIMAPClient.SelectedFolder;
+end;
+
 procedure TCustomMail.GetFolderList(var ListFolders: TStringList);
+// Получить список директорий на сервере
+var
+  i: integer;
 begin
   fIMAPClient.List('', ListFolders);
+  for i := 0 to ListFolders.Count - 1 do
+    ListFolders[i] := CharsetConversion(ListFolders[i], TMimeChar.UTF_7mod, TMimeChar.UTF_8);
 end;
 
 constructor TCustomMail.Create;
+// Создание обьекта
 begin
   inherited Create;
   fIMAPClient := TIMAPSend.Create;
   fIMAPClient.AutoTLS := True;
   fIMAPClient.FullSSL := True;
   fConnected := False;
-  fLogin := 'i.rcode';
+  fUserName := 'i.rcode';
   fPassword := 'LQexIX1CSS';
   fIMAPHost := 'imap.yandex.ru';
   fIMAPPort := 993;
+  fCurrentFolderUIID := 0;
+  fCountOfNewMails := 0;
+  fCountOfMailsInCurrentFolder := 0;
 end;
 
 destructor TCustomMail.Destroy;
+// Уничтожение обьекта
 begin
   fIMAPClient.Logout;
   fIMAPClient.Free;
@@ -80,4 +171,3 @@ begin
 end;
 
 end.
-
--- Тестовые программы/Работа с почтой/main.pas
+++ Тестовые программы/Работа с почтой/main.pas
@@ -13,7 +13,6 @@ type
   { TMainForm }
 
   TMainForm = class(TForm)
-    Button1: TButton;
     EditPortOut: TLabeledEdit;
     EditUserName: TLabeledEdit;
     EditUserPassword: TLabeledEdit;
@@ -21,13 +20,25 @@ type
     EditPortIn: TLabeledEdit;
     MainMenu: TMainMenu;
     Memo: TMemo;
-    procedure Button1Click(Sender: TObject);
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
+    Panel1: TPanel;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure MenuItem2Click(Sender: TObject);
+    procedure MenuItem3Click(Sender: TObject);
+    procedure MenuItem4Click(Sender: TObject);
+    procedure MenuItem5Click(Sender: TObject);
   private
     procedure SetLogLine(AValue: string);
     { private declarations }
   public
     { public declarations }
     mail: TCustomMail;
+    procedure BeginNewFunc;
     property Log: string write SetLogLine;
   end;
 
@@ -40,25 +51,72 @@ implementation
 
 { TMainForm }
 
-procedure TMainForm.Button1Click(Sender: TObject);
+procedure TMainForm.FormCreate(Sender: TObject);
+// Создание
+begin
+  mail := TCustomMail.Create;
+end;
+
+procedure TMainForm.FormDestroy(Sender: TObject);
+// Уничтожение
+begin
+  mail.Free;
+end;
+
+procedure TMainForm.MenuItem2Click(Sender: TObject);
+// Получить список папок
 var
   list: TStringList;
+  str: string;
 begin
-
-  mail := TCustomMail.Create;
-  mail.Connected := True;
-  Log := BoolToStr(mail.Connected, 'Connected', 'Error');
-  Log := mail.FullResult;
-
-  list:= TStringList.Create;
+  BeginNewFunc;
+  list := TStringList.Create;
   mail.GetFolderList(list);
-  Memo.Lines.Assign(list);
+  for str in list do
+    Memo.Lines.Add(str);
   list.Free;
 end;
 
+procedure TMainForm.MenuItem3Click(Sender: TObject);
+// Выбрать папку
+begin
+  BeginNewFunc;
+  mail.SelectedFolder := 'INBOX';
+  Log := mail.FullResult.Text;
+end;
+
+procedure TMainForm.MenuItem4Click(Sender: TObject);
+// Подключиться
+begin
+  BeginNewFunc;
+  mail.Connected := True;
+  Log := BoolToStr(mail.Connected, 'Connected', 'Error');
+  Log := mail.FullResult.Text;
+end;
+
+procedure TMainForm.MenuItem5Click(Sender: TObject);
+// Узнать количество новых писем
+begin
+  BeginNewFunc;
+  Log := IntToStr(mail.CountOfNewMails);
+end;
+
 procedure TMainForm.SetLogLine(AValue: string);
+// Занести в лог
 begin
   Memo.Append(AValue);
+end;
+
+procedure TMainForm.BeginNewFunc;
+// Разделитель для красоты
+var
+  i: integer;
+  buf: string;
+begin
+  buf := '';
+  for i := 0 to 40 do
+    buf := buf + '#';
+  log := buf;
 end;
 
 end.
